8e12f56f34aa3f22ad2865c7634db6b3b318bc79
Kim Lesieur <lesieurkim@gmail.com>
2022-03-29 12:08:02 +0200

    Adding URL query filter by category to getProducts

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,18 +1,25 @@
 const {pool} = require('../database');
 
-const getProducts = async () => {
+const getProducts = async (id = '') => {
+    if(id){
+        const categoryId = parseInt(id);
+        return pool
+        .query(`SELECT * FROM products WHERE category_id = $1`, [categoryId])
+        .then(res => res.rows)
+        .catch(err => console.error('Error while executing a getProducts query', err.stack))
+    }
     return pool
-    .query(`SELECT * FROM products ORDER BY id`, [])
-    .then(res => res.rows)
-    .catch(err => console.error('Error while executing a getProducts query', err.stack))
+        .query(`SELECT * FROM products ORDER BY id`, [])
+        .then(res => res.rows)
+        .catch(err => console.error('Error while executing a getProducts query', err.stack))
 };
 
 const getProductById = async (id) => {
     console.log(`voilà ton id : ${id}`);
     return pool
-    .query(`SELECT * FROM products WHERE id = $1`, [id])
-    .then(res => res.rows[0])
-    .catch(err => console.error('Error while executing a getProductById query', err.stack))
+        .query(`SELECT * FROM products WHERE id = $1`, [id])
+        .then(res => res.rows[0])
+        .catch(err => console.error('Error while executing a getProductById query', err.stack))
 };
 
 module.exports = {getProducts, getProductById};
--- routes/products.js
+++ routes/products.js
@@ -3,9 +3,13 @@ const { getProducts, getProductById } = require('../models/productsModel');
 const productsRouter = express.Router();
 
 productsRouter.get('/', async (req, res, next) => {
+    if (req.query.category) {
+        const products = await getProducts(req.query.category);
+        console.log(products);
+        return res.status(200).send(products);
+    }
     const products = await getProducts();
-    console.log(products);
-    res.status(200).send(products);
+    return res.status(200).send(products);
 });
 
 productsRouter.get('/:id', async (req, res,next) => {
@@ -13,6 +17,8 @@ productsRouter.get('/:id', async (req, res,next) => {
     const product = await getProductById(id);
     res.status(200).send(product);
 });
+
+
 
 
 module.exports = productsRouter;
