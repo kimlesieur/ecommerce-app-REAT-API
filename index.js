fcb3c86fa4b4fb29dc6d1b736c3afe5e83424df1
Kim Lesieur <lesieurkim@gmail.com>
2022-03-30 17:10:42 +0200

    Add express-session and a middleware to control authentication

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const session = require("express-session");
+
 const {PORT} = require('./config');
 const productsRouter = require('./routes/products');
 const accountRouter = require('./routes/account');
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+const users = [
+  {
+    id: 1,
+    email: "sam",
+    password: "p",
+  },
+  {
+    id: 2,
+    email: "jill",
+    password: "p",
+  },
+];
+
+
 /* 
 //Test to pass a date variable through the sub-Routers
 const timeMiddleware = (req, res, next) => {  
@@ -16,8 +30,93 @@ const timeMiddleware = (req, res, next) => {
 app.use(timeMiddleware);
 */
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(
+  session({
+    name: 'kim-cookie',
+    secret: "f4z4gs$Gcg",
+    cookie: { maxAge: 300000000, secure: false },
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+const ensureAuthentication = (req, res, next) => {
+  if (req.session.userId) {
+    return next();
+  } else {
+    console.log("You're not authorized to view this page" );
+    res.redirect('/');
+  }
+}
+
+app.get('/', (req, res, next) => {
+  console.log(req.session);
+  res.send(
+    `<h1>Welcome!<h1>
+    <a href='/login'>Login</a>
+    <a href='/register'>Register</a>
+    <a href='/'>Home</a>
+    <form method='post' action='/logout'>
+      <button>Logout</button>
+    </form>`
+  )
+});
+
+app.get('/login', (req, res, next) => {
+  res.send(`
+    <h1>Login</h1>
+    <form method='post' action='/login'>
+      <input name='email' placeholder='Email' require/>
+      <input type='password' name='password' placeholder='Password' require/> 
+      <input type='submit' />
+    </form>
+    <a href='/register'>Register</a>
+  `
+  )
+});
+
+app.get('/register', (req, res, next) => {
+ res.send(`
+ <h1>Register</h1>
+ <form method='post' action='/register'>
+  <input name='name' placeholder='Name' require/>
+   <input name='email' placeholder='Email' require/>
+   <input type='password' name='password' placeholder='Password' require/> 
+   <input type='submit' />
+ </form>
+ <a href='/register'>Login</a>
+ `)
+});
+
+app.post('/login', (req, res, next) => {
+  const {email, password} = req.body;
+  if(email && password){
+    const user = users.find(user => user.email === email && user.password === password )
+    if(user){
+      req.session.userId = user.id;
+      return res.redirect('/account')
+    }
+  }
+  res.redirect('/login')
+});
+
+
+app.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if(err){
+      return res.redirect('/home')
+    }
+    res.clearCookie('kim-cookie');
+    res.redirect('/login');
+  })
+});
+
 app.use('/products', productsRouter);
-app.use('/account', accountRouter);
+app.use('/account', ensureAuthentication, accountRouter);
 
 
 
@@ -26,3 +125,7 @@ app.use('/account', accountRouter);
 
 
 
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
